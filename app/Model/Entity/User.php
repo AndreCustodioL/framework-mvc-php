ec5955daeb11b1d85416fc2c0390ac35b0f7a28b
AndreCustodioL <andre.custodio@outlook.com>
2024-07-28 11:52:21 -0300

    Adicionado Autenticação de Usuários
    Adicionado autenticação de usuário para proteger o painel admin do projeto, implementado middlewares para tornar obrigatório o login do usuário em certas rotas ou tornar obrigatório o logout do usuário em certas rotas

---
+++ app/Model/Entity/User.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Model\Entity;
+
+use \WilliamCosta\DatabaseManager\Database;
+
+class User{
+    
+    /**
+     * ID do usuário
+     *
+     * @var int
+     */
+    public $id;
+    
+    /**
+     * Nome do usuário
+     *
+     * @var string
+     */
+    public $nome;
+    
+    /**
+     * Email do usuário
+     *
+     * @var string
+     */
+    public $email;
+    
+    /**
+     * Senha do usuário
+     *
+     * @var string
+     */
+    public $senha;
+    
+    /**
+     * Método responsável por retornar um usuário com base em seu email
+     *
+     * @param  string $email
+     * @return User
+     */
+    public static function getUserByEmail($email){
+        return (new Database('usuarios'))->select('email ="'.$email.'"')->fetchObject(self::class);
+    }
+
+    
+
+}
